20e3fdc40e0aa0b7002a2fcab8e138ee7bcd38b5
Luke Colvin <97006355+lucolvin@users.noreply.github.com>
2023-11-06 22:50:47 -0600

    Delete gitV2.bat

--- gitV2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\lukec\OneDrive - Olivet Nazarene University\Documents\College 2023-\Obsidian-Backup"
-start /min "" cmd /c "git add --all & git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%" & git push"
-exit
